675c76abb577bc78a0cb9d4e4d2fed921477cce3
Victor <victor_correra@hotmail.com>
2021-01-29 13:24:29 +0000

    Create top_40_adv_rate_with_cedant.sql

---
+++ Queries/Unrated and ADV Buyers/top_40_adv_rate_with_cedant.sql
@@ -0,0 +1,55 @@
+SELECT
+    ULTIMATE_ID,
+    ULTIMATE_NAME,
+    ULTIMATE_ISO_COUNTRY,
+    MODEL_SUB_TYPE,
+    CUSTOMER_ID,
+    POD,
+    EXPOSURE,
+    ECAP
+FROM
+    (
+        SELECT
+            *
+        FROM
+            (
+                SELECT
+                    *
+                FROM
+                    (
+                        SELECT
+                            ULTIMATE_ID
+                        FROM
+                            CALCXXXX.SO_REPORTING
+                        GROUP BY
+                            ULTIMATE_ID
+                        ORDER BY
+                            SUM(
+                                CASE
+                                    WHEN MODEL_SUB_TYPE LIKE '%_COM_KN'
+                                    AND ULTIMATE_RATING_TYPE LIKE 'ADV'
+                                    AND MODEL_TYPE LIKE 'IR' THEN CREDIT_LIMIT_NET_EXPOSURE
+                                    ELSE 0
+                                END
+                            ) DESC
+                    )
+                WHERE
+                    ROWNUM <= 40
+            ) new
+            LEFT JOIN (
+                select
+                    ULTIMATE_ID AS ULTIMATE_ID_HELPER,
+                    ULTIMATE_NAME,
+                    ULTIMATE_ISO_COUNTRY,
+                    MODEL_SUB_TYPE,
+                    CUSTOMER_ID,
+                    ULTIMATE_POD as POD,
+                    CREDIT_LIMIT_NET_EXPOSURE as EXPOSURE,
+                    EC_CONSUMPTION_ND as ECAP
+                from
+                    CALCXXXX.SO_REPORTING
+                where
+                    MODEL_TYPE LIKE 'IR'
+                    and ULTIMATE_RATING_TYPE LIKE 'ADV'
+            ) old ON new.ULTIMATE_ID = old.ULTIMATE_ID_HELPER
+    )
