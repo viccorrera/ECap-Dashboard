fa5f576d90ded5db3313d6e7801a0b4d5bebd50f
Victor <victor_correra@hotmail.com>
2021-02-11 16:55:42 +0000

    Update Credit_movements.sql
    Minor changes to query

--- Queries/Cedant/Credit_movements.sql
+++ Queries/Cedant/Credit_movements.sql
@@ -1,11 +1,8 @@
 SELECT
-    CASE
-        WHEN t.COMP_ID_PREV IS NULL THEN t.COMP_ID_CURR
-        ELSE t.COMP_ID_PREV
-    END AS COMP_ID,
-    ECAP_PREV_QRT,
-    ECAP_CURR_QRT,
-    ECAP_CURR_QRT - ECAP_PREV_QRT as DELTA
+    NVL(OLD_COMP_ID, NEW_COMP_ID) AS COMP_ID,
+    OLD_ECAP,
+    NEW_ECAP,
+    NVL(NEW_ECAP, 0) - NVL(OLD_ECAP, 0) AS DELTA_ECAP
 FROM
     (
         SELECT
@@ -13,10 +10,10 @@ FROM
         FROM
             (
                 SELECT
-                    SUBSTR(CONTRACT_ID, 0, 5) AS COMP_ID_PREV,
-                    SUM(EC_CONSUMPTION_ND) AS ECAP_PREV_QRT
+                    SUBSTR(CONTRACT_ID, 0, 5) AS OLD_COMP_ID,
+                    SUM(EC_CONSUMPTION_ND) AS OLD_ECAP
                 FROM
-                    CALCXXXX.SO_REPORTING -- should correspond to previous quarter
+                    CALC6673.SO_REPORTING -- UPDATE OLD QUARTER --
                 WHERE
                     MODEL_TYPE = 'IR'
                     AND MODEL_SUB_TYPE LIKE 'CI_%'
@@ -24,14 +21,14 @@ FROM
                     SUBSTR(CONTRACT_ID, 0, 5)
             ) old FULL OUTER JOIN (
                 SELECT
-                    SUBSTR(CONTRACT_ID, 0, 5) AS COMP_ID_CURR,
-                    SUM(EC_CONSUMPTION_ND) AS ECAP_CURR_QRT
+                    SUBSTR(CONTRACT_ID, 0, 5) AS NEW_COMP_ID,
+                    SUM(EC_CONSUMPTION_ND) AS NEW_ECAP
                 FROM
-                    CALCXXXX.SO_REPORTING -- should correspond to current quarter
+                    CALC6724.SO_REPORTING -- UPDATE NEW QUARTER --
                 WHERE
                     MODEL_TYPE = 'IR'
                     AND MODEL_SUB_TYPE LIKE 'CI_%'
                 GROUP BY
                     SUBSTR(CONTRACT_ID, 0, 5)
-            ) new ON old.COMP_ID_PREV = new.COMP_ID_CURR
-    ) t
+            ) new ON old.OLD_COMP_ID = new.NEW_COMP_ID
+    )
