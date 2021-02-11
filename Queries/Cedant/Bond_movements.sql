828264b5e75f3c3800044f50f4080fad3fcbbe49
Victor <victor_correra@hotmail.com>
2021-02-11 16:57:09 +0000

    Update Bond_movements.sql
    Minor changes to query

--- Queries/Cedant/Bond_movements.sql
+++ Queries/Cedant/Bond_movements.sql
@@ -1,16 +1,8 @@
 SELECT
-    CASE
-        WHEN COMP_ID_PREV IS NULL THEN COMP_ID_CURR
-        ELSE COMP_ID_PREV
-    END AS COMP_ID,
-    CASE
-        WHEN ECAP_PREV_QRT IS NULL THEN 0
-        ELSE ECAP_PREV_QRT
-    END AS ECAP_PREV_QRT,
-    CASE
-        WHEN ECAP_CURR_QRT IS NULL THEN 0
-        ELSE ECAP_CURR_QRT
-    END AS ECAP_CURR_QRT
+    NVL(OLD_COMP_ID, NEW_COMP_ID) AS COMP_ID,
+    OLD_ECAP,
+    NEW_ECAP,
+    NVL(NEW_ECAP, 0) - NVL(OLD_ECAP, 0) AS DELTA_ECAP
 FROM
     (
         SELECT
@@ -18,10 +10,10 @@ FROM
         FROM
             (
                 SELECT
-                    SUBSTR(CONTRACT_ID, 0, 5) AS COMP_ID_PREV,
-                    SUM(EC_CONSUMPTION_ND) AS ECAP_PREV_QRT
+                    SUBSTR(CONTRACT_ID, 0, 5) AS OLD_COMP_ID,
+                    SUM(EC_CONSUMPTION_ND) AS OLD_ECAP
                 FROM
-                    CALCXXXX.SO_REPORTING -- should correspond to previous quarter
+                    CALCXXXX.SO_REPORTING -- UPDATE OLD QUARTER --
                 WHERE
                     MODEL_TYPE = 'IR'
                     AND MODEL_SUB_TYPE LIKE 'BO_%'
@@ -29,14 +21,14 @@ FROM
                     SUBSTR(CONTRACT_ID, 0, 5)
             ) old FULL OUTER JOIN (
                 SELECT
-                    SUBSTR(CONTRACT_ID, 0, 5) AS COMP_ID_CURR,
-                    SUM(EC_CONSUMPTION_ND) AS ECAP_CURR_QRT
+                    SUBSTR(CONTRACT_ID, 0, 5) AS NEW_COMP_ID,
+                    SUM(EC_CONSUMPTION_ND) AS NEW_ECAP
                 FROM
-                    CALCXXXX.SO_REPORTING -- should correspond to current quarter
+                    CALCXXXX.SO_REPORTING -- UPDATE NEW QUARTER --
                 WHERE
                     MODEL_TYPE = 'IR'
                     AND MODEL_SUB_TYPE LIKE 'BO_%'
                 GROUP BY
                     SUBSTR(CONTRACT_ID, 0, 5)
-            ) new ON old.COMP_ID_PREV = new.COMP_ID_CURR
+            ) new ON old.OLD_COMP_ID = new.NEW_COMP_ID
     )
